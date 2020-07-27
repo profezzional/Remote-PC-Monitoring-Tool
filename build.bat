d8551740a077efd3e50d51d4387576b1857e90ce
colton <cnpearson42@gmail.com>
2020-07-27 01:55:23 -0500

    updated build scripts to detect python env

--- build.bat
+++ build.bat
@@ -1,1 +1,6 @@
+python --version 2>NUL
+if errorlevel 1 goto error
 python scripts\build.py
+goto:eof
+:errorNoPython
+echo Python does not appear to be installed, looks like you'll have to compile manually.
--- build.sh
+++ build.sh
@@ -1,1 +1,8 @@
-python scripts/build.py
+if which python > /dev/null 2>&1;
+then
+    python scripts/build.py
+else
+    echo "Python does not appear to be installed, looks like you'll have to compile manually."
+fi
+
+
